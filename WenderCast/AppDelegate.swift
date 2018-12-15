6cb5b80a5a33c4cc4a8d5c41e55080bdd1405640
zgpeace <zgpeace@gmail.com>
2018-12-15 12:19:49 +0800

    推送配置、Action Notification、静默推送 实现

--- WenderCast/AppDelegate.swift
+++ WenderCast/AppDelegate.swift
@@ -29,6 +29,7 @@
  */
 import UIKit
 import SafariServices
+import UserNotifications
 
 fileprivate let viewActionIdentifier = "VIEW_IDENTIFIER"
 fileprivate let newsCategoryIdentifier = "NEWS_CATEGORY"
@@ -40,11 +41,145 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    UNUserNotificationCenter.current().delegate = self
     
     UITabBar.appearance().barTintColor = UIColor.themeGreenColor
     UITabBar.appearance().tintColor = UIColor.white
+    registerForPushNotification()
+    
+    // Check if launched from notification
+    let notificationOption = launchOptions?[.remoteNotification]
+    if let notification = notificationOption as? [String: AnyObject],
+    let aps = notification["aps"] as? [String: AnyObject]
+    {
+        NewsItem.makeNewsItem(aps)
+        (window?.rootViewController as? UITabBarController)?.selectedIndex = 1
+    }
     
     return true
   }
+    
+    func registerForPushNotification() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, error in
+                print("Permission granted: \(granted)")
+                guard granted else {
+                    // Permission is not granted, show alert user can go to setting page
+                    
+                    return
+                }
+                
+                let viewAction = UNNotificationAction(identifier: viewActionIdentifier, title: "View", options: [.foreground])
+                let newsCategory = UNNotificationCategory(identifier: newsCategoryIdentifier, actions: [viewAction], intentIdentifiers: [], options: [])
+                UNUserNotificationCenter.current().setNotificationCategories([newsCategory])
+                
+                self?.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+            completionHandler(.failed)
+            return
+        }
+        
+        // 1
+        if aps["content-available"] as? Int == 1{
+             let podcastStore = PodcastStore.sharedStore
+            // 2
+            podcastStore.refreshItems { (didLoadNewItems) in
+                // 3
+                completionHandler(didLoadNewItems ? .newData : .noData)
+            }
+        } else {
+            // 4
+            NewsItem.makeNewsItem(aps)
+            completionHandler(.newData)
+        }
+        
+    }
+    
 }
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // 1
+        let userInfo = response.notification.request.content.userInfo
+        
+        // 2
+        if let aps = userInfo["aps"] as? [String: AnyObject],
+            let newsItem = NewsItem.makeNewsItem(aps)
+        {
+            (window?.rootViewController as? UITabBarController)?.selectedIndex = 1
+            
+            // 3
+            if response.actionIdentifier == viewActionIdentifier,
+                let url = URL(string: newsItem.link) {
+                let safari = SFSafariViewController(url: url)
+                window?.rootViewController?.present(safari, animated: true, completion: nil)
+                
+            }
+            
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
